3cbe6737af87a8ad56a2562703c32662035a0a80
menja-coder <rakotomenja@gmail.com>
2020-05-22 13:39:25 +0300

    debug bouton se deconnecter et mailer

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   devise_for :clients, controllers: {
   	confirmations: 'confirmations' 
   }
+
+  devise_scope :client do  
+   get '/clients/sign_out' => 'devise/sessions#destroy'     
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
