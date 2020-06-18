51adb21fb3936c0966137cde5e06e6cbb551417d
menja-coder <rakotomenja@gmail.com>
2020-06-18 14:37:11 +0300

    relation menu-resto ok

--- app/models/resto.rb
+++ app/models/resto.rb
@@ -1,2 +1,7 @@
 class Resto < ApplicationRecord
+
+	   #relation resto N-N menu
+    has_many :menu_restos
+    has_many :menus, through: :menu_restos
+
 end
---
+++ test/models/menu_resto_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MenuRestoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
